10fcb7a77865680530e6d85328e79021fb0c0047
Rohit <nathrohit48@gmail.com>
2025-08-11 00:02:38 +0530

    implemented Spring Scecurity Social login using github and facebook.

---
+++ src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -0,0 +1,41 @@
+package com.eazybytes.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ProjectSecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests((requests) -> requests.requestMatchers("/secure").authenticated()
+                        .anyRequest().permitAll())
+                .formLogin(Customizer.withDefaults())
+                .oauth2Login(Customizer.withDefaults());
+        return httpSecurity.build();
+    }
+
+    @Bean
+    ClientRegistrationRepository clientRegistrationRepository() {
+        ClientRegistration github = githubClientRegistration();
+        ClientRegistration facebook = facebookClientRegistration();
+        return new InMemoryClientRegistrationRepository(github, facebook);
+    }
+
+    private ClientRegistration githubClientRegistration() {
+        return CommonOAuth2Provider.GITHUB.getBuilder("github").clientId("Ov23livqvBjkcWjrj057")
+                .clientSecret("58aab52ac4295b7ae3aa90591f82676a4ba32f9d").build();
+    }
+
+    private ClientRegistration facebookClientRegistration() {
+        return CommonOAuth2Provider.FACEBOOK.getBuilder("facebook").clientId("1990171295059355")
+                .clientSecret("61b3f88efe9f1a058ce75b1d9c0495f4").build();
+    }
+}
